0c6d44fbfc6e1193d78ae8d479c894934e0e1e5e
Kevin Lee <kevinmlee.x@gmail.com>
2014-02-15 16:01:41 -0800

    added right click cycling, fixed x marking causing offset

--- light.js
+++ light.js
@@ -1,23 +1,35 @@
 $(document).ready(function() {
+
+	$('.tile').click(function() {
+		updateTileColor($(this), 1);
+	});
+
+	$('.tile').bind("contextmenu", function(e) {
+		event.preventDefault();
+		updateTileColor($(this), -1);
+	});
+});
+
+function updateTileColor(elem, step) {
 	// cycling
 	var roomtypes = ['blank', 'normal', 'treasure', 'store',
 	'boss', 'arcade', 'library', 'challenge'];
-
-	$('.tile').click(function() {
-		// update tile color
-		for (var i = 0; i < roomtypes.length; i++) {
-			if ($(this).hasClass(roomtypes[i])) {
-				$(this).removeClass(roomtypes[i])
-					.addClass(roomtypes[(i+1)%roomtypes.length]);
-				break; // otherwise we cycle all of them!
-			}
+	
+	// update tile color
+	for (var i = 0; i < roomtypes.length; i++) {
+		if (elem.hasClass(roomtypes[i])) {
+			elem.removeClass(roomtypes[i])
+				.addClass(roomtypes[ 
+					i+1*step >= 0 ? (i+1*step)%roomtypes.length : roomtypes.length - 1
+					]);
+			break; // otherwise we cycle all of them!
 		}
+	}
 
-		// mark last with x
-		if ($(this).parent().parent().data('lastClicked')) {
-			$(this).parent().parent().data('lastClicked').text('');
-		}
-		$(this).parent().parent().data('lastClicked', $(this));
-		$(this).text('x');
-	});
-});
+	// mark last with x
+	if (elem.parent().parent().data('lastClicked')) {
+		elem.parent().parent().data('lastClicked').text('');
+	}
+	elem.parent().parent().data('lastClicked', elem);
+	elem.text('x');
+}
